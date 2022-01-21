b22b77da11a303653f15bc02ca2f7554ca851955
Melissa Chaca <chacamelissad@gmail.com>
2022-01-20 23:43:52 -0600

    add forms

--- app/forms.py
+++ app/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class RegisterForm(FlaskForm):
-    first_name = StringField('First Name', validators = [DataRequired()])
-    last_name = StringField('Last Name', validators = [DataRequired()])
+    firstname = StringField('First Name', validators = [DataRequired()])
+    lastname = StringField('Last Name', validators = [DataRequired()])
     address = StringField('Address', validators = [DataRequired()])
-    phone_number = StringField('Phone Number', validators = [DataRequired()])
+    phonenumber = StringField('Phone Number', validators = [DataRequired()])
     submit = SubmitField('Register')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app 
 from flask import render_template
+from app.forms import RegisterForm
 
 @app.route('/')
 def index():
@@ -11,6 +12,9 @@ def name():
     my_name= 'Melissa'
     return f"Hello {my_name}"
 
-@app.route('/addcontact')
-def test():
-    return "this is a test"
+@app.route('/register', methods= ["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        print("FORM  HAS BEEN VLIDATED")
+    return render_template('register.html', form=form)
